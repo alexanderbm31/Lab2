d51a68db3ccef823ffa71e6d70e391a4e70fc13c
alexanderbm31 <alexanderbm31@gmail.com>
2019-02-11 15:10:16 -0600

    Mensaje

---
+++ App2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("App2.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f4fd488d-72b1-4207-aff3-8b9454d7bd2c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ App2.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using App2.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App2.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        double resultadoEsperado;
+        double resultadoObtenido;
+
+        [TestMethod]
+        public void TestSuma()
+        {
+            Calculos calculos = new Calculos();
+            resultadoEsperado = 10;
+            resultadoObtenido = calculos.Suma(3, 7);
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido);
+        }
+    }
+}
