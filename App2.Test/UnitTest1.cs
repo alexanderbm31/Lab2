bcacaaf858c9a8de8cf925db6f155b6da8e64ba7
alexanderbm31 <abarrantes@impesa.net>
2019-02-22 16:49:04 -0600

    Update UnitTest1.cs

--- App2.Test/UnitTest1.cs
+++ App2.Test/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace App2.Test
         {
             Calculos calculos = new Calculos();
             resultadoEsperado = 10;
-            resultadoObtenido = calculos.Suma(3, 1);//f
+            resultadoObtenido = calculos.Suma(3, 1);//fg
 
             Assert.AreEqual(resultadoEsperado, resultadoObtenido);
         }
